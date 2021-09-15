a4e9cf1649256271a1b3ea5e935da4d4013caaab
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-15 17:47:28 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+intents.typing = True
+intents.presences = True
+
+bot = commands.Bot("~", intents=intents)
+
+cogs = ["botcogs."+cog[:-3] for cog in os.listdir("./botcogs") if cog[-3:] == ".py"]
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+    print("Loading cogs. . .")
+    for cog in cogs:
+        try:
+            bot.load_extension(cog)
+            print(cog + " was loaded.")
+        except Exception as e:
+            print(e)
+
+token = ""
+bot.run(token)
