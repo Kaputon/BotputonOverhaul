90aaf32a596f8001ef282f878f421206d137e29f
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-15 17:45:49 -0400

    serverpost.py

---
+++ botcogs/serverpost.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+class ServerPost(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command()
+    async def serverpost(self, ctx, *args:str):
+        channels = [ch for ch in ctx.guild.text_channels if not ch.name in args]
+
+        count = 0
+        try:
+            with ctx.typing():
+                for chan in channels:
+                    async for _ in chan.history(limit=None):
+                        count+=1
+            embed = discord.Embed(title="Overall Posts", description=f"{count} posts.")
+            embed.set_author(icon_url=self.bot.user.avatar_url, name=ctx.guild.name)
+            embed.set_thumbnail(url=ctx.guild.icon_url)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(e)
+
+def setup(bot):
+    bot.add_cog(ServerPost(bot))
+    
