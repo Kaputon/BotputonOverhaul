0085e1be914b1bd22ab44cbff8c526e03d9aeae3
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-15 17:46:57 -0400

    botConfig.py

---
+++ botConfig.py
@@ -0,0 +1,9 @@
+
+class Assets:
+    StrawPollIMG = "https://cdn.discordapp.com/attachments/389514146053488671/805006169919455232/220.png"
+    text_channel_blacklist = ["kaputon-tf2-text"]
+
+def get_StrawPollIMG():
+    return Assets.StrawPollIMG
+def get_blacklisted_channels():
+    return Assets.text_channel_blacklist
