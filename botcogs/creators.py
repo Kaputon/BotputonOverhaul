1ed2c5c34c071c0cfca075f476b4ee8579897a2d
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-15 22:17:14 -0400

    Update creators.py

--- botcogs/creators.py
+++ botcogs/creators.py
@@ -60,7 +60,8 @@ class Creators(commands.Cog):
             map = str(tag.find('td', class_="server-list-col centered-text map-cover").text).strip("\n")
             area = str(tag.find('td', class_="server-list-col centered-text",
                                 style="padding: 0 1rem")).split("|")
-            area = [are for are in area if are in AREAS][0]
+            area = [location for location in area if location.strip() in AREAS]
+            area = area[0]
 
             code = tag.find('td', class_="server-list-col flex")
             if code:
