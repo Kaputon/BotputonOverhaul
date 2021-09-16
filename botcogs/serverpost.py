079d53b5433d53110ca874292672670eee7ef47a
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-15 22:53:58 -0400

    serverpost.py commented

--- botcogs/serverpost.py
+++ botcogs/serverpost.py
@@ -8,18 +8,18 @@ class ServerPost(commands.Cog):
 
     @commands.command()
     async def serverpost(self, ctx, *args:str):
-        channels = [ch for ch in ctx.guild.text_channels if not ch.name in args]
+        channels = [ch for ch in ctx.guild.text_channels if not ch.name in args] # Grab each channel in the server if not blacklisted by the user.
 
         count = 0
         try:
             with ctx.typing():
-                for chan in channels:
+                for chan in channels: # For each message in each channel, increase the count.
                     async for _ in chan.history(limit=None):
                         count+=1
             embed = discord.Embed(title="Overall Posts", description=f"{count} posts.")
             embed.set_author(icon_url=self.bot.user.avatar_url, name=ctx.guild.name)
             embed.set_thumbnail(url=ctx.guild.icon_url)
-            await ctx.send(embed=embed)
+            await ctx.send(embed=embed) # Send it.
         except Exception as e:
             await ctx.send(e)
 
