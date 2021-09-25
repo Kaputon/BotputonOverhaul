a581edababad0f2dad9d4c2b65ec21f831659601
Kaputon <73202553+Kaputon@users.noreply.github.com>
2021-09-24 21:52:54 -0400

    now using databases

--- botcogs/serverpost.py
+++ botcogs/serverpost.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import MongoHandler
 
 class ServerPost(commands.Cog):
 
@@ -7,22 +8,15 @@ class ServerPost(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def serverpost(self, ctx, *args:str):
-        channels = [ch for ch in ctx.guild.text_channels if not ch.name in args] # Grab each channel in the server if not blacklisted by the user.
+    async def serverp(self, ctx):
 
-        count = 0
-        try:
-            with ctx.typing():
-                for chan in channels: # For each message in each channel, increase the count.
-                    async for _ in chan.history(limit=None):
-                        count+=1
-            embed = discord.Embed(title="Overall Posts", description=f"{count} posts.")
-            embed.set_author(icon_url=self.bot.user.avatar_url, name=ctx.guild.name)
-            embed.set_thumbnail(url=ctx.guild.icon_url)
-            await ctx.send(embed=embed) # Send it.
-        except Exception as e:
-            await ctx.send(e)
+        g_ID = ctx.guild.id
+        mongClient = MongoHandler.ClientMongo()
+
+        embed = discord.Embed(title="Overall Posts", description=f"{mongClient.returnServer(g_ID)} posts.")
+        embed.set_author(icon_url=self.bot.user.avatar_url, name=ctx.guild.name)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(ServerPost(bot))
-    
